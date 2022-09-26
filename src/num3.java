da7451065fcb22a01cf806aef5498c28f7ec362e
Rumine <114250490+Rum1ne@users.noreply.github.com>
2022-09-26 20:30:11 +0300

    Initial commit

--- src/num3.java
+++ src/num3.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class num3 {
     public static void main(String[] args) {
-
+        Scanner seconds = new Scanner(System.in);
+        int n = seconds.nextInt();
+        String hour = (n / 3600) + ":";
+        int min = n / 60 % 60;
+        int sec = n % 60;
+        String m = "00";
+        if (min < 10) {
+            m = "0" + min + ":";                      //некрасивый код вышел :с
+        } else {
+            m = min + ":";
+        }
+        String s = "00";
+        if (sec < 10) {
+            s = "0" + sec;
+        } else {
+            s = sec + " ";
+        }
+        System.out.println(hour + m + s);
     }
 }
--- src/num4.java
+++ src/num4.java
@@ -1,5 +1,9 @@
-class Main {
-    public static void main (String[] args) {
-
+import java.util.Locale;
+import java.util.Scanner;
+public class num4 {
+    public static void main(String[] args) {
+        Scanner dot = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        double n = dot.nextDouble();
+        System.out.println((-2<=n && n<=3 || 6<=n && n<=9)? false : true);
     }
 }
