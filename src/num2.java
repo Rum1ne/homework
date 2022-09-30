5a4b4f95e0a359f32246fcee2439de760f4fe325
Rumine <114250490+Rum1ne@users.noreply.github.com>
2022-09-30 15:57:18 +0300

    Initial commit

--- src/num2.java
+++ src/num2.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class num2 {
     public static void main(String[] args) {
-        Scanner all = new Scanner(System.in);
-        int r = all.nextInt();
-        int k = all.nextInt();
-        int n = all.nextInt();
-        System.out.println((r * n + k * n/100) + " " + (k * n%100));
+
 
     }
 }
--- src/num4.java
+++ src/num4.java
@@ -2,8 +2,6 @@ import java.util.Locale;
 import java.util.Scanner;
 public class num4 {
     public static void main(String[] args) {
-        Scanner dot = new Scanner(System.in).useLocale(Locale.ENGLISH);
-        double n = dot.nextDouble();
-        System.out.println((-2<=n && n<=3 || 6<=n && n<=9)? false : true);
+
     }
 }
