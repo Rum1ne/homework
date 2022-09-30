087955ea2c86e0e394d2c84432b3b850d1670ab4
Rumine <114250490+Rum1ne@users.noreply.github.com>
2022-09-30 18:41:48 +0300

    Initial commit

--- src/num1.java
+++ src/num1.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 public class num1 {
     public static void main(String[] args) {
-        // an example
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        int a = 1, b = 1;
+        if (n == 1 || n == 2) {
+            System.out.println(1);
+        }
+        else {
+            for (int i = 0; i < n-2; ++i) {
+                if (a<b) {
+                    a=a+b;
+                }
+                else {
+                    b=b+a;
+                }
+            }
+            System.out.println(Math.max(a, b));
+        }
     }
 }
--- src/num4.java
+++ src/num4.java
@@ -1,7 +1,19 @@
-import java.util.Locale;
 import java.util.Scanner;
 public class num4 {
     public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        int i = 9;
+        while (n>0) {
+            if (i>n%10 && n%10!=0) {
+                i = n%10;
+                n /=10;
+            }
+            else {
+                n /=10;
+            }
+        }
+        System.out.println(i);
 
     }
 }
