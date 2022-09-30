5fa3c9a9190c7d1def4c31a1cdd1bed2d000c17a
Rumine <114250490+Rum1ne@users.noreply.github.com>
2022-09-30 15:55:24 +0300

    Initial commit

--- src/num1.java
+++ src/num1.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 public class num1 {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        int n = num.nextInt();
-        int m = n/100 + n/10%10 + n%10;
-        System.out.println(m + " " + (m + 2 - n%2));
+        // an example
     }
 }
