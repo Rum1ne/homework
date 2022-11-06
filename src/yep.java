5dde4c3d7eb224dda219720e6257662df8656c64
Rumine <114250490+Rum1ne@users.noreply.github.com>
2022-11-07 01:20:01 +0300

    Initial commit

--- src/yep.java
+++ src/yep.java
@@ -1,42 +1,38 @@
 import java.util.Scanner;
 
-/*
-Может ли покемон с введёными характеристиками победить на одной из стадий эволюций
-Что он скажет на второй стадии при победе/поражение
-Кританёт ли он на третьей стадии вообще..
-
-Данные для проверки кода
-Pikachu 10 400 100 10000 4 Nah! 70 270
-Ivi 70 1200 50 80000 8 bruh 90 190
-*/
 public class yep {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Pokemon first = new Pokemon();
-        first.setName(in.next());                                        //имя покемона
-        first.setPokemonpower(in.nextInt());                             //сила покемона
-        first.setPokemonhp(in.nextInt());                                //здоровье покемона
-        first.setEnemypower(in.nextInt());                               //сила противника
-        first.setEnemyhp(in.nextInt());                                  //здоровье противника
-        NicePokemon second = new NicePokemon();
-        second.name = first.name;
-        second.pokemonpower = first.pokemonpower * 8;
-        second.pokemonhp = first.pokemonhp * 4;
-        second.enemypower = first.enemypower;
-        second.enemyhp = first.enemyhp;
-        second.setUlt(in.nextInt());                                      //количество атак необходимое для заряжания ульты
-        BestPokemon third = new BestPokemon();
-        third.name = first.name;
-        third.pokemonpower = second.pokemonpower * 12;
-        third.pokemonhp = second.pokemonhp * 6;
-        third.enemypower = first.enemypower;
-        third.enemyhp = first.enemyhp;
-        third.ult = second.ult;
-        third.setNoise(in.next());                                        //звук при крите покемона
-        third.setCritChance(in.nextInt());                                //шанс крита
-        third.setCritDamage(in.nextInt());                                //процент дополнительного урона
-        first.boom();
-        second.boom();
-        third.boom();
+        System.out.println("Fraction1:");
+        Fraction fraction1 = new Fraction(18, 30);
+        System.out.println(fraction1);
+        System.out.println(fraction1.toDecimal());
+
+        System.out.println();
+        System.out.println("Fraction2:");
+        Fraction fraction2 = new Fraction(7);
+        System.out.println(fraction2);
+        System.out.println(fraction2.toDecimal());
+
+        System.out.println();
+        System.out.println("Fraction3:");
+        Fraction fraction3 = new Fraction();
+        System.out.println(fraction3);
+        System.out.println(fraction3.toDecimal());
+
+        System.out.println();
+        System.out.println("Fraction4");
+        Fraction fraction4 = new Fraction("12/7");
+        System.out.println(fraction4);
+        System.out.println(fraction4.toDecimal());
+
+        System.out.println();
+        System.out.println("Fraction 5");
+        Fraction fraction5 = new Fraction(12.06);
+        System.out.println(fraction5);
+        System.out.println(fraction5.toDecimal(12.06));
+
+
+
     }
 }
